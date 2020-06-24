a3d7a6111ae5f74cd17e74a4bb74e3ca768015f1
Nighthawk1210 <nighthawk1210coc@gmail.com>
2020-06-25 05:18:07 +0530

    button

--- src/containers/ShipsContainer.js
+++ src/containers/ShipsContainer.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React , {useState} from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { Ship } from '../components/Ship'
 import GET_SHIPS from '../graphql/get-ship'
 
 function ShipsContainer() {
 
+    const[count , setCount] = useState(10)
+
     const {data: {ships = []} = {} } = useQuery(GET_SHIPS, {
-        variables: { limit: 10},
+        variables: { limit : count},
 
     });
+
+    function increment() {
+        setCount(prevCount => prevCount + 1)
+    }
+    
+    function decrement() {
+        setCount(prevCount => prevCount - 1)
+    }
 
 
     return(
         <div className="container">
+            <button onClick={decrement}>Show less</button>
             {ships && ships.map(ship => <Ship key={ship.id} ship={ship} />)}
+            <button onClick={increment}>Show More</button>
         </div>
+        
     );
 }
 
