e1b120201f7bfa9675dc029364ff43124fc07601
Prithvish Baidya <deformercoding@gmail.com>
2020-06-25 05:40:15 +0530

    Added filter

--- src/containers/ShipsContainer.js
+++ src/containers/ShipsContainer.js
@@ -1,33 +1,34 @@
-import React , {useState} from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { Ship } from '../components/Ship'
 import GET_SHIPS from '../graphql/get-ship'
 
 function ShipsContainer() {
 
-    const[count , setCount] = useState(10)
-
-    const {data: {ships = []} = {} } = useQuery(GET_SHIPS, {
-        variables: { limit : count},
+    const [count, setCount] = useState(10)
+    const [shipName, setShipName] = useState("");
+    const { data: { ships = [] } = {} } = useQuery(GET_SHIPS, {
+        variables: { limit: count },
 
     });
 
     function increment() {
         setCount(prevCount => prevCount + 1)
     }
-    
+
     function decrement() {
         setCount(prevCount => prevCount - 1)
     }
 
 
-    return(
+    return (
         <div className="container">
+            <input type="text" value={shipName} onChange={(event) => setShipName(event.target.value)} />
             <button onClick={decrement}>Show less</button>
-            {ships && ships.map(ship => <Ship key={ship.id} ship={ship} />)}
+            {ships && ships.filter(ship => ship.name.toLowerCase().includes(shipName.toLowerCase())).map(ship => <Ship key={ship.id} ship={ship} />)}
             <button onClick={increment}>Show More</button>
         </div>
-        
+
     );
 }
 
